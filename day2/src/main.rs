e432d262a7a68cd335fd48b50b86d6240790110e
aol1306 <jdzieciatko@gmail.com>
2022-12-02 07:14:17 +0100

    Updated to use lazy_static

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,4 +1,23 @@
+use lazy_static::lazy_static;
 use std::collections::HashMap;
+
+lazy_static! {
+static ref WIN_TABLE: HashMap<&'static str, &'static str> = {
+    let mut m = HashMap::new();
+    m.insert("C", "A"); // to defeat C, choose A (rock defeats scissors)
+    m.insert("B", "C");
+    m.insert("A", "B");
+    m
+};
+
+static ref LOSE_TABLE: HashMap<&'static str, &'static str> = {
+    let mut m = HashMap::new();
+    m.insert("A", "C"); // to lose to A, choose C (rock defeats scissors)
+    m.insert("C", "B");
+    m.insert("B", "A");
+    m
+};
+}
 
 // calculate score for a rock-paper-scissors game
 fn calculate_score(round_shapes: &(&str, &str)) -> u64 {
@@ -10,12 +29,9 @@ fn calculate_score(round_shapes: &(&str, &str)) -> u64 {
         _ => panic!(),
     };
     // points for result - only winning combinations
-    score += match round_shapes {
-        ("C", "A") => 6, // rock defeats scissors
-        ("B", "C") => 6, // scissors defeats paper
-        ("A", "B") => 6, // paper defeats rock
-        _ => 0,
-    };
+    if WIN_TABLE.get(round_shapes.0).unwrap() == &round_shapes.1 {
+        score += 6;
+    }
     // points for a draw
     if round_shapes.0 == round_shapes.1 {
         score += 3;
@@ -31,7 +47,7 @@ fn test_calculate_score() {
 }
 
 // part 1 - assume X = A, Y = B, Z = C
-fn calculate_round_shapes_1<'a>(round: &'a(&'a str, &'a str)) -> (&'a str, &'a str) {
+fn calculate_round_shapes_1<'a>(round: &'a (&'a str, &'a str)) -> (&'a str, &'a str) {
     match round.1 {
         "X" => (round.0, "A"),
         "Y" => (round.0, "B"),
@@ -46,25 +62,14 @@ fn test_calculate_round_shapes_1() {
 }
 
 // part 2 - X -> we have to lose, Y -> draw, Z -> win
-fn calculate_round_shapes_2<'a>(round: &'a(&'a str, &'a str)) -> (&'a str, &'a str) {
-    // https://docs.rs/bimap/latest/bimap/
-    let mut win_table = HashMap::new();
-    win_table.insert("C", "A"); // to defeat C, choose A (rock defeats scissors)
-    win_table.insert("B", "C");
-    win_table.insert("A", "B");
-
-    let mut lose_table = HashMap::new();
-    lose_table.insert("A", "C"); // to lose to A, choose C (rock defeats scissors)
-    lose_table.insert("C", "B");
-    lose_table.insert("B", "A");
-
+fn calculate_round_shapes_2<'a>(round: &'a (&'a str, &'a str)) -> (&'a str, &'a str) {
     match round.1 {
         // lose - choose losing shape
-        "X" => (round.0, lose_table.get(round.0).unwrap()),
+        "X" => (round.0, LOSE_TABLE.get(round.0).unwrap()),
         // draw - choose the same shape as the opponent
         "Y" => (round.0, round.0),
         // win - choose winning shape
-        "Z" => (round.0, win_table.get(round.0).unwrap()),
+        "Z" => (round.0, WIN_TABLE.get(round.0).unwrap()),
         _ => panic!(),
     }
 }
@@ -83,10 +88,7 @@ fn main() {
         .map(|x| {
             // convert to pairs
             let mut s = x.split(" ");
-            (
-                s.next().unwrap(),
-                s.next().unwrap(),
-            )
+            (s.next().unwrap(), s.next().unwrap())
         })
         .collect::<Vec<_>>();
 
