a6ed1585a043fbb01c6aa5787c8d70e58b56d37a
aol1306 <jdzieciatko@gmail.com>
2022-12-04 07:14:03 +0100

    Add day4

---
+++ day4/src/main.rs
@@ -0,0 +1,135 @@
+#[derive(Debug)]
+struct AssignmentPair {
+    elf1: Assignment,
+    elf2: Assignment,
+}
+
+impl TryFrom<&str> for AssignmentPair {
+    type Error = &'static str;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let mut split = value.split(",");
+        let elf1 = split.next().ok_or("invalid input")?;
+        let elf2 = split.next().ok_or("invalid input")?;
+
+        Ok(AssignmentPair {
+            elf1: Assignment::try_from(elf1)?,
+            elf2: Assignment::try_from(elf2)?,
+        })
+    }
+}
+
+impl AssignmentPair {
+    fn is_self_contained(&self) -> bool {
+        self.elf1.contains(&self.elf2) || self.elf2.contains(&self.elf1)
+    }
+
+    fn is_overlapping(&self) -> bool {
+        self.elf1.overlaps(&self.elf2)
+    }
+}
+
+#[derive(Debug)]
+struct Assignment {
+    start: u64,
+    end: u64,
+}
+
+impl TryFrom<&str> for Assignment {
+    type Error = &'static str;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let mut split = value.split("-");
+        let start = split
+            .next()
+            .ok_or("invalid input")?
+            .parse::<u64>()
+            .map_err(|_| "parse failed")?;
+        let end = split
+            .next()
+            .ok_or("invalid input")?
+            .parse::<u64>()
+            .map_err(|_| "parse failed")?;
+
+        Ok(Assignment {
+            start: start,
+            end: end,
+        })
+    }
+}
+
+impl Assignment {
+    fn contains(&self, other: &Assignment) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+
+    fn overlaps(&self, other: &Assignment) -> bool {
+        (self.end >= other.start && self.start <= other.end)
+            || (other.end <= self.start && other.start >= self.end)
+    }
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    let part1 = input
+        .lines()
+        .map(|line| AssignmentPair::try_from(line).unwrap())
+        .map(|pair| pair.is_self_contained())
+        .filter(|result| *result)
+        .count();
+
+    println!("answer 1: {part1}");
+
+    let part2 = input
+        .lines()
+        .map(|line| AssignmentPair::try_from(line).unwrap())
+        .map(|pair| pair.is_overlapping())
+        .filter(|result| *result)
+        .count();
+
+    println!("answer 2: {part2}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_assignment_contains() {
+        let a = Assignment::try_from("2-8").unwrap();
+        let b = Assignment::try_from("3-7").unwrap();
+        assert_eq!(a.contains(&b), true);
+        assert_eq!(b.contains(&a), false);
+    }
+
+    #[test]
+    fn test_assignment_overlaps() {
+        let a = Assignment::try_from("5-7").unwrap();
+        let b = Assignment::try_from("7-9").unwrap();
+        assert_eq!(a.overlaps(&b), true);
+        let a = Assignment::try_from("2-4").unwrap();
+        let b = Assignment::try_from("6-8").unwrap();
+        assert_eq!(a.overlaps(&b), false);
+    }
+
+    #[test]
+    fn test_assignment_pair_is_self_contained() {
+        let p = AssignmentPair::try_from("6-6,4-6").unwrap();
+        assert_eq!(p.is_self_contained(), true);
+        let p = AssignmentPair::try_from("2-3,4-5").unwrap();
+        assert_eq!(p.is_self_contained(), false);
+    }
+
+    #[test]
+    fn test_assignment_pair_is_overlapping() {
+        let p = AssignmentPair::try_from("2-3,4-5").unwrap();
+        assert_eq!(p.is_overlapping(), false);
+        let p = AssignmentPair::try_from("2-6,4-8").unwrap();
+        assert_eq!(p.is_overlapping(), true);
+
+        let p = AssignmentPair::try_from("3-4,1-2").unwrap();
+        assert_eq!(p.is_overlapping(), false);
+    }
+}
