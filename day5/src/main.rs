d95b0a22640b55d5e25d5f8dd491ac3c07b31971
aol1306 <jdzieciatko@gmail.com>
2022-12-05 17:40:43 +0100

    Add tests

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -5,7 +5,7 @@ lazy_static! {
     static ref RE: Regex = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Storage {
     stacks: Vec<Stack>,
 }
@@ -42,8 +42,8 @@ impl TryFrom<&str> for Storage {
 }
 
 impl Storage {
-    fn execute_movement_instruction(&mut self, movement_instuction: &str) {
-        let caps = RE.captures(movement_instuction).unwrap();
+    fn execute_movement_instruction(&mut self, movement_instruction: &str) {
+        let caps = RE.captures(movement_instruction).unwrap();
         let count = caps[1].parse::<usize>().unwrap();
         let from = caps[2].parse::<usize>().unwrap() - 1;
         let to = caps[3].parse::<usize>().unwrap() - 1;
@@ -62,8 +62,8 @@ impl Storage {
         self.stacks[to].crates.push(moved_crate);
     }
 
-    fn execute_movement_instruction_9001(&mut self, movement_instuction: &str) {
-        let caps = RE.captures(movement_instuction).unwrap();
+    fn execute_movement_instruction_9001(&mut self, movement_instruction: &str) {
+        let caps = RE.captures(movement_instruction).unwrap();
         let count = caps[1].parse::<usize>().unwrap();
         let from = caps[2].parse::<usize>().unwrap() - 1;
         let to = caps[3].parse::<usize>().unwrap() - 1;
@@ -86,14 +86,20 @@ impl Storage {
         }
         ret
     }
+
+    fn top_crates_pretty(&self) -> String {
+        self.get_top_crates()
+            .iter()
+            .fold(String::new(), |acc, x| acc + &x.id.to_string())
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Stack {
     crates: Vec<Crate>,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Crate {
     id: char,
 }
@@ -111,13 +117,7 @@ fn main() {
             storage.execute_movement_instruction(line);
         }
 
-        println!(
-            "answer 1: {}",
-            storage
-                .get_top_crates()
-                .iter()
-                .fold(String::new(), |acc, x| acc + &x.id.to_string())
-        );
+        println!("answer 1: {}", storage.top_crates_pretty());
     }
     // part 2
     {
@@ -126,12 +126,85 @@ fn main() {
             storage.execute_movement_instruction_9001(line);
         }
 
-        println!(
-            "answer 2: {}",
-            storage
-                .get_top_crates()
-                .iter()
-                .fold(String::new(), |acc, x| acc + &x.id.to_string())
-        );
+        println!("answer 2: {}", storage.top_crates_pretty());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    lazy_static! {
+        static ref TEST_STORAGE: Storage = Storage {
+            stacks: vec![
+                Stack {
+                    crates: vec![Crate { id: 'Z' }, Crate { id: 'N' }],
+                },
+                Stack {
+                    crates: vec![Crate { id: 'M' }, Crate { id: 'C' }, Crate { id: 'D' }],
+                },
+                Stack {
+                    crates: vec![Crate { id: 'P' }],
+                },
+            ],
+        };
+    }
+
+    #[test]
+    fn test_storage_from() {
+        let test_str = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 ";
+        println!("{}", test_str);
+
+        assert_eq!(Storage::try_from(test_str).unwrap(), *TEST_STORAGE);
+    }
+
+    #[test]
+    fn test_storage_execute_movement() {
+        let mut storage = TEST_STORAGE.clone();
+        storage.execute_movement_instruction("move 2 from 2 to 3");
+        assert_eq!(storage.stacks[2].crates.last().unwrap(), &Crate{id: 'C'});
+    }
+
+    #[test]
+    fn test_storage_execute_movement_9001() {
+        let mut storage = TEST_STORAGE.clone();
+        storage.execute_movement_instruction_9001("move 2 from 1 to 3");
+        assert_eq!(storage.stacks[2].crates.last().unwrap(), &Crate{id: 'N'});
+    }
+
+    #[test]
+    fn test_storage_top_crates_pretty() {
+        assert_eq!(TEST_STORAGE.top_crates_pretty(), "NDP");
+    }
+
+    #[test]
+    fn test_all() {
+        let input = include_str!("input.txt");
+        let input_parts = input.split("\n\n").collect::<Vec<_>>();
+        let storage = input_parts[0];
+        let movements = input_parts[1];
+
+        // part 1
+        {
+            let mut storage = Storage::try_from(storage).unwrap();
+            for line in movements.lines() {
+                storage.execute_movement_instruction(line);
+            }
+
+            assert_eq!("RLFNRTNFB", storage.top_crates_pretty());
+        }
+        // part 2
+        {
+            let mut storage = Storage::try_from(storage).unwrap();
+            for line in movements.lines() {
+                storage.execute_movement_instruction_9001(line);
+            }
+
+            assert_eq!("MHQTLJRLB", storage.top_crates_pretty());
+        }
     }
 }
